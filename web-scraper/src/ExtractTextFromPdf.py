c72dfe167b0528c52e05a1017f2ba91f1b0cb817
mklycs <michael.klys@gmail.com>
2024-11-15 12:09:07 +0100

    added pdf donwload functionality

--- web-scraper/src/ExtractTextFromPdf.py
+++ web-scraper/src/ExtractTextFromPdf.py
@@ -2,6 +2,17 @@ import json
 import PyPDF2
 import os
 
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
 def extract_text_from(filename):
 
     try:
@@ -77,5 +88,76 @@ def clear_cache():
 
 #clear_cache()
 
+def get_data():
+    response = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+    soup = BeautifulSoup(response.content, "html.parser") 
+    return soup.find("tbody")
+
+def parse_companies(entire_table):
+    companies = []
+    for table_row in entire_table.findAll("tr")[1:]: 
+        columns = table_row.findAll("td")
+        company = columns[0].get_text(strip=True)
+        companies.append(company)
+    return companies
+
+def setup_driver():
+    download_directory_path = os.path.abspath(os.getcwd())[:-3] + "data"
+
+    ChromeDriverManager().install()
+    chrome_download_configs = {
+        "download.default_directory": download_directory_path,
+        "download.prompt_for_download": False, 
+        "download.directory_upgrade": True,
+        "plugins.always_open_pdf_externally": True
+    }
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option('prefs', chrome_download_configs)
+    return webdriver.Chrome(options=chrome_options)
+
+def download_reports(companies, driver):
+    wait = WebDriverWait(driver, 1)
+
+    counter = 1
+    for company in companies:
+        if counter >= 9: break
+        try:
+            driver.get("https://www.responsibilityreports.com")
+
+            search_bar = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > div.container > section.banner_section > div > div > div.left_section > form > input[type=text]:nth-child(2)")))
+            search_bar.send_keys(company) # here the element of the company list but you can use for example "AAPL" as well
+            search_bar.submit()
+
+            link = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > div.container > section.category_section > div.apparel_stores_company_list > ul > li:nth-child(2) > span:nth-child(1) > a")))
+            link.click()
+
+            try:
+                first_report = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > div.container > section > div.profile_content_block > div.right_section > div.most_recent_block > div.most_recent_content_block > div.view_btn > a")))
+                first_report.click()
+
+                try:
+                    popup = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "body > div.container > div > div > div.close_popup > a")))
+                    popup.click()
+                except:
+                    pass
+            except Exception as excetpion:
+                pass
+
+        except Exception as exception:
+            print(f"{exception}")
+            continue
+
+        counter += 1
+
+def scrape():
+    data = get_data()
+    companies = parse_companies(data)
+
+    driver = setup_driver()
+    download_reports(companies, driver)
+
+    driver.quit()
+
 if __name__ == "__main__":
-    process_data()
+    scrape()
+    process_data() # need to be adjusted to the webscraping process
